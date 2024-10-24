439e83ef92e8086f04412c1f6c67dcd854a33141
Junius Oblea <67751041+jiroblea@users.noreply.github.com>
2024-10-24 17:00:24 +0800

    Merge (#13) branch to development: functional buttons for screen navigation (#20)
    working buttons: play, modes, about, and each back button of the mentioned screens

--- iskrambol/main.py
+++ iskrambol/main.py
@@ -3,8 +3,10 @@ kivy.require('2.3.0')
 
 from kivy.app import App
 from kivy.config import Config
+from kivy.lang.builder import Builder
+from kivy.uix.screenmanager import ScreenManager, Screen
 
-from kivy.uix.boxlayout import BoxLayout
+from playscreen import PlayScreen
 
 # screen size are based on Galaxy S20 (most common screen size for mobile)
 Config.set('graphics', 'resizable', False)
@@ -12,16 +14,59 @@ Config.set('graphics', 'width', 360)
 Config.set('graphics', 'height', 800)
 
 
-class MainPage(BoxLayout):
+def load_kv_files():
+    Builder.load_file('iskrambol.kv')
+    Builder.load_file('playscreen.kv')
+    Builder.load_file('modesscreen.kv')
+    Builder.load_file('aboutscreen.kv')
+
+
+class WindowManager(ScreenManager):
+    """Required class for window navigation."""
     pass
 
 
-class Iskrambol(App):
-    
+# screens:
+class MainScreen(Screen):
+    """Main menu screen."""
+    pass
+
+
+PlayScreen()
+
+
+class ModesScreen(Screen):
+    pass
+
+
+class AboutScreen(Screen):
+    pass
+
+
+class IskrambolApp(App):
+    """
+    App object.
+    """
+
+
     def build(self):
-        root = MainPage()
-        return root
+
+        screen_manager = WindowManager()
+        screens = [
+            MainScreen(name='main_screen'),
+            PlayScreen(name='play_screen'),
+            ModesScreen(name='modes_screen'),
+            AboutScreen(name='about_screen')
+        ]
+
+        for screen in screens:
+            screen_manager.add_widget(screen)
+
+        screen_manager.current = 'main_screen'
+
+        return screen_manager
 
 
 if __name__ == '__main__':
-    Iskrambol().run()
+    load_kv_files()
+    IskrambolApp().run()
