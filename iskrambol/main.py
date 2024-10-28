380d92ef59cc528724672288fdee0213a3217eae
jiroblea <jirooblea@gmail.com>
2024-10-29 00:32:42 +0800

    created UI of modes screen

--- iskrambol/main.py
+++ iskrambol/main.py
@@ -32,9 +32,6 @@ class MainScreen(Screen):
     pass
 
 
-PlayScreen()
-
-
 class ModesScreen(Screen):
     pass
 
@@ -62,7 +59,7 @@ class IskrambolApp(App):
         for screen in screens:
             screen_manager.add_widget(screen)
 
-        screen_manager.current = 'main_screen'
+        screen_manager.current = 'modes_screen'
 
         return screen_manager
 
