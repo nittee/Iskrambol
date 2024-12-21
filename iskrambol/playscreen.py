24b1f94ffc25162ef1653283d4ace4fa2b1a95fc
jiroblea <jirooblea@gmail.com>
2024-12-21 21:50:25 +0800

    added working pause popup UI

--- iskrambol/playscreen.py
+++ iskrambol/playscreen.py
@@ -1,5 +1,16 @@
 from kivy.uix.screenmanager import Screen
+from kivy.uix.popup import Popup
+from kivy.core.window import Window
 
 
 class PlayScreen(Screen):
-    pass
+    def show_pause_popup(self):
+        # show = PauseWindow()
+        global pausePopup
+        pausePopup = Popup(title="Pause", content=PauseWindow(), size_hint=(0.8,0.6))
+        pausePopup.open()
+
+
+class PauseWindow(Screen):
+    def close_pause_popup(self):
+        pausePopup.dismiss()
