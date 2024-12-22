ee67cc7f3e7e6ddabe2c899c874031c9ce6e1cbe
Junius Oblea <67751041+jiroblea@users.noreply.github.com>
2024-12-22 10:08:07 +0800

    Merged '29 pause button pop up' branch to development (#30)
    * added working pause popup UI
    
    * clean up

--- iskrambol/playscreen.py
+++ iskrambol/playscreen.py
@@ -1,5 +1,14 @@
 from kivy.uix.screenmanager import Screen
+from kivy.uix.popup import Popup
 
 
 class PlayScreen(Screen):
-    pass
+    def show_pause_popup(self):
+        global pausePopup
+        pausePopup = Popup(title="Pause", content=PauseWindow(), size_hint=(0.8,0.6))
+        pausePopup.open()
+
+
+class PauseWindow(Screen):
+    def close_pause_popup(self):
+        pausePopup.dismiss()
